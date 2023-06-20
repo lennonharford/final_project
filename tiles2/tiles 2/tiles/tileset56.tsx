aac827bc37d173527e31a27d93112a4972eeee13
lennonharford <lennonh45@kprschools.ca>
2023-06-20 10:37:57 -0400

    x

--- tiles2/tiles 2/tiles/tileset56.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="tileset56" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="6" columns="3">
- <image source="tileset56.png" width="52" height="35"/>
-</tileset>
